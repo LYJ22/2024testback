78ba88a04cb4af72ed36ec0f08dc409b1aba7852
Lee Yu Jeong(이유정) <79090053+LYJ22@users.noreply.github.com>
2024-10-11 16:14:16 +0900

    food service

---
+++ src/main/java/com/sample/spring/api/FoodApi.java
@@ -0,0 +1,65 @@
+package com.sample.spring.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.spring.api.request.CreateAndEditFoodRequest;
+import com.sample.spring.api.response.FoodDetailView;
+import com.sample.spring.api.response.FoodView;
+import com.sample.spring.model.FoodEntity;
+import com.sample.spring.service.FoodService;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class FoodApi {
+	
+	@Autowired
+	private FoodService foodservice;
+	
+	@GetMapping("/foods")
+	public List<FoodView> getFoods() {
+		return foodservice.getAllFoods();
+	}
+	
+	@GetMapping("/food/{foodId}")
+	public FoodDetailView viewFoods(
+			@PathVariable("foodId") Long foodId
+			) {
+		return foodservice.getFoodDetail(foodId);
+	}
+	
+	@PostMapping("/food")
+	public FoodEntity postFood(
+			@RequestBody CreateAndEditFoodRequest request
+			){
+		return foodservice.createFood(request);
+//		return "postFood / name: " + request.getName() + ", address: " + request.getAddress() +", 메뉴[0]: " + request.getMenus().get(1).getName();
+	}
+	
+	@PutMapping("/food/{foodId}")
+	public String editFood(
+			@PathVariable("foodId") Long foodId,
+			@RequestBody CreateAndEditFoodRequest request
+			) {
+		foodservice.editFood(foodId, request);
+		return "editFood"+ foodId + ", / name: " + request.getName() + ", address: " + request.getAddress();
+	}
+	
+	@DeleteMapping("/food/{foodId}")
+	public void deleteFood(
+			@PathVariable("foodId") Long foodId
+			) {
+		foodservice.deleteFood(foodId);
+	}
+}
---
+++ src/main/java/com/sample/spring/service/FoodService.java
@@ -0,0 +1,122 @@
+package com.sample.spring.service;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sample.spring.api.request.CreateAndEditFoodRequest;
+import com.sample.spring.api.response.FoodDetailView;
+import com.sample.spring.api.response.FoodView;
+import com.sample.spring.model.FoodEntity;
+import com.sample.spring.model.MenuEntity;
+import com.sample.spring.repository.FoodRepository;
+import com.sample.spring.repository.MenuRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class FoodService {
+	@Autowired
+	private FoodRepository foodrepository;
+	
+	@Autowired
+	private MenuRepository menurepository;
+	
+	@Transactional
+	public FoodEntity createFood(
+			CreateAndEditFoodRequest request
+			) {
+		FoodEntity food = FoodEntity.builder()
+				.name(request.getName())
+				.address(request.getAddress())
+				.createAt(ZonedDateTime.now())
+				.updateAt(ZonedDateTime.now())
+				.build();
+		
+		foodrepository.save(food);
+		
+		request.getMenus().forEach((menu)->{
+			MenuEntity menuEntity = MenuEntity.builder()
+					.foodId(food.getId())
+					.name(menu.getName())
+					.price(menu.getPrice())
+					.createAt(ZonedDateTime.now())
+					.updateAt(ZonedDateTime.now())
+					.build();
+			
+			menurepository.save(menuEntity);
+		});
+		
+		return food;
+	}
+	
+	@Transactional
+	public void editFood(
+			Long foodId,
+			CreateAndEditFoodRequest request
+			) {
+		FoodEntity food = foodrepository.findById(foodId).orElseThrow(()->new RuntimeException("no food"));
+		food.changeNameAndAddress(request.getName(), request.getAddress());
+		foodrepository.save(food);
+		
+		List<MenuEntity> menus = menurepository.findAllByFoodId(foodId);
+		menurepository.deleteAll(menus);
+		
+		request.getMenus().forEach((menu)->{
+			MenuEntity menuEntity = MenuEntity.builder()
+					.foodId(food.getId())
+					.name(menu.getName())
+					.price(menu.getPrice())
+					.createAt(ZonedDateTime.now())
+					.updateAt(ZonedDateTime.now())
+					.build();
+			
+			menurepository.save(menuEntity);
+		});
+	}
+	
+	public void deleteFood(Long foodId) {
+		FoodEntity food = foodrepository.findById(foodId).orElseThrow();
+		foodrepository.delete(food);
+		
+		List<MenuEntity> menus = menurepository.findAllByFoodId(foodId);
+		menurepository.deleteAll(menus);
+	}
+	
+	public List<FoodView> getAllFoods(){
+		List<FoodEntity> foods = foodrepository.findAll();
+		return foods.stream().map((food)->
+			FoodView.builder()
+			.id(food.getId())
+			.name(food.getName())
+			.address(food.getAddress())
+			.createdAt(food.getCreateAt())
+			.updatedAt(food.getUpdateAt())
+			.build()
+		).toList();
+	}
+	
+	public FoodDetailView getFoodDetail(Long foodId) {
+		FoodEntity food = foodrepository.findById(foodId).orElseThrow();
+		
+		List<MenuEntity> menus = menurepository.findAllByFoodId(foodId);
+		return FoodDetailView.builder()
+				.id(food.getId())
+				.name(food.getName())
+				.address(food.getAddress())
+				.createdAt(food.getCreateAt())
+				.updatedAt(food.getUpdateAt())
+				.menus(menus.stream().map((menu)->
+					FoodDetailView.Menu.builder()
+					.id(menu.getFoodId())
+					.name(menu.getName())
+					.price(menu.getPrice())
+					.createdAt(menu.getCreateAt())
+					.updatedAt(menu.getUpdateAt())
+					.build()
+					).toList())
+				.build();
+	}
+}
